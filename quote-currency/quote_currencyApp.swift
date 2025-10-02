474769e7962efe9583532357cf6ac4cb5e6a357d
tborjal <tborjal@byupathway.edu>
2025-10-02 21:30:54 +0800

    Initial Commit

---
+++ quote-currency/quote_currencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  quote_currencyApp.swift
+//  quote-currency
+//
+//  Created by Terence Dreico Borjal on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct quote_currencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
